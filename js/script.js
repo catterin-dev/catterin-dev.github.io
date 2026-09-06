17e81dd619255bb9ac20729ce8c64d49f4bef5ef
CATTERINE CARELY QUISPE QUISPE <109066616+catterin-dev@users.noreply.github.com>
2026-09-05 20:11:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,3 +47,16 @@ function escribirTexto() {
 }
 
 escribirTexto();
+
+/* =====================================================
+   FONDO DINÁMICO CON EL CURSOR
+   ===================================================== */
+
+const cursorLight = document.querySelector(".cursor-light");
+
+document.addEventListener("mousemove", (e) => {
+
+    cursorLight.style.left = `${e.clientX}px`;
+    cursorLight.style.top = `${e.clientY}px`;
+
+});
